64f0a877c9ac80c6585ac50d3b0aaaeee997356a
Sumkor <Sumkor@foxmail.com>
2021-07-14 00:03:57 +0800

    init

---
+++ src/test/java/com/sumkor/MyTest.java
@@ -0,0 +1,16 @@
+package com.sumkor;
+
+import org.junit.Test;
+
+/**
+ * @author Sumkor
+ * @since 2021/7/14
+ */
+public class MyTest {
+
+    @Test
+    public void test() {
+        System.out.println("hello world");
+    }
+
+}
